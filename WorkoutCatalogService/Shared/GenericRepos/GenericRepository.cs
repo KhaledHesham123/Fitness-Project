2adc7528d47531246024cf623ffb016ce47da985
Eng Khaled <Eng Khaled@DESKTOP-9QN4FUV>
2025-11-08 17:17:22 +0200

    2

---
+++ WorkoutCatalogService/Shared/GenericRepos/GenericRepository.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq;
+using WorkoutCatalogService.Data.Context;
+using WorkoutCatalogService.Shared.Entites;
+
+namespace WorkoutCatalogService.Shared.GenericRepos
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly WorkoutCatalogDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+        public IDbContextTransaction? _Transaction { get; set; }
+
+        public GenericRepository(WorkoutCatalogDbContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbContext.Set<T>().Where(e=>e.IsDeleted == false).AsQueryable();
+        }
+
+        public IQueryable<T> GetByIdQueryable(Guid id)
+        {
+            return _dbContext.Set<T>().Where(e =>e.Id == id&&e.IsDeleted==false).AsQueryable();
+
+
+        }
+        public async Task addAsync(T item)
+        {
+            await _dbContext.Set<T>().AddAsync(item);
+        }
+
+        public  Task UpdateAsync(T item)
+        {
+            _dbContext.Set<T>().Update(item);
+            return Task.CompletedTask;
+        }
+
+        public void SaveInclude(T entity)
+        {
+            var existingEntity = _dbSet.Local.FirstOrDefault(e => e.Id == entity.Id);
+            if (existingEntity == null) 
+            {
+                existingEntity = _dbSet.AsNoTracking().FirstOrDefault(e => e.Id == entity.Id);
+                if (existingEntity == null)
+                    throw new Exception($"Entity of type {typeof(T).Name} with Id {entity.Id} not found.");
+
+                _dbSet.Attach(existingEntity);
+
+            }
+
+            var entry = _dbContext.Entry(existingEntity);
+            var keyNames = entry.Metadata.FindPrimaryKey().Properties.Select(p => p.Name).ToList();
+
+
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                if (entry.Metadata.FindProperty(prop.Name) == null)
+                    continue;
+
+                if (keyNames.Contains(prop.Name))
+                    continue;
+
+                var oldvalue = prop.GetValue(existingEntity);
+                var newvale = prop.GetValue(entity);
+
+                if (newvale != null && !object.Equals(oldvalue, newvale))
+                {
+                    prop.SetValue(existingEntity, newvale);
+                    entry.Property(prop.Name).IsModified = true;
+                }
+                else
+                {
+                    entry.Property(prop.Name).IsModified = false;
+                }
+
+
+            }
+
+
+        }
+
+
+        public Task DeleteAsync(T item)
+        {
+            _dbContext.Set<T>().Remove(item);
+            return Task.CompletedTask;
+        }
+
+
+
+        public Task<int> SaveChanges()
+        {
+            return _dbContext.SaveChangesAsync();
+
+        }
+
+       
+        
+    }
+}
---
+++ WorkoutCatalogService/Shared/Response/EndpointResponse.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WorkoutCatalogService.Shared.Response
+{
+    public class EndpointResponse<T>
+    {
+        private T Data { get; set; }
+        private string Message { get; set; }
+        private bool IsSuccess { get; set; }
+        private int StatusCode { get; set; }
+
+        public EndpointResponse(T data, int statusCode = 200, string message = "", bool isSuccess = true)
+        {
+            Data = data;
+            StatusCode = statusCode;
+            Message = message;
+            IsSuccess = isSuccess;
+        }
+        public EndpointResponse( int statusCode = 200, string message = "", bool isSuccess = true)
+        {
+            
+            StatusCode = statusCode;
+            Message = message;
+            IsSuccess = isSuccess;
+        }
+
+       
+
+        public static EndpointResponse<T> Success(T data, string message = "", int statusCode = 200)
+        {
+            return new EndpointResponse<T>(data, statusCode,message, true);
+        }
+
+        public static EndpointResponse<T> Fail(string message, int statusCode)
+        {
+            return new EndpointResponse<T>( statusCode, message, true);
+        }
+
+
+    }
+}
