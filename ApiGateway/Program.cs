f47e53681621d93efead818eb51ac3ac814774a2
Eng Khaled <Eng Khaled@DESKTOP-9QN4FUV>
2025-11-09 22:27:59 +0200

    aa

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -5,7 +5,12 @@ namespace ApiGateway
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
             var app = builder.Build();
+
+            app.MapReverseProxy();  
 
             app.MapGet("/", () => "Hello World!");
 
