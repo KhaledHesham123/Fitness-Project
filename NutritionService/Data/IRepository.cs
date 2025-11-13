c923df6e404aae86739324ab84d870ccf8324d0a
KholoudEbrahim <khokoudebrahim419@gmail.com>
2025-11-13 20:55:00 +0200

    IRepository

---
+++ NutritionService/Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using NutritionService.Models;
+using System.Linq.Expressions;
+
+namespace NutritionService.Data
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+      
+        T Add(T entity);
+        T Update(T entity);
+        Task DeleteAsync(int id);
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Get(Expression<Func<T, bool>> expression);
+        void SaveInclude(T entity, params string[] includedProperties);
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
