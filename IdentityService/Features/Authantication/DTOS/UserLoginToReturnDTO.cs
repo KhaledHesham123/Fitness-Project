663cb84b08dd2422846dd9f9c634f89ea9974aaa
Eng Khaled <Eng Khaled@DESKTOP-9QN4FUV>
2025-11-14 23:03:22 +0200

    aa

---
+++ IdentityService/Features/Authantication/DTOS/UserLoginToReturnDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityService.Features.Authantication.DTOS
+{
+    public class UserLoginToReturnDTO
+    {
+        
+
+        public string role { get; set; }
+        public string Token { get; set; }
+    }
+}
