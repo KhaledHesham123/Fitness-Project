6bfbc1ea075f151bb765359484b1a8d753fcaeca
Youssefabdulaty <youssef.ys665@gmail.com>
2025-11-18 03:25:34 +0200

    create register and login

---
+++ IdentityService/Features/Authantication/Queries/GetUserByRefreshToken/GetUserByRefreshTokenQuery.cs
@@ -0,0 +1,8 @@
+﻿using IdentityService.Features.Shared;
+using IdentityService.Shared.Entities;
+using MediatR;
+
+namespace IdentityService.Features.Authantication.Queries.GetUserByRefreshToken
+{
+    public record GetUserByRefreshTokenQuery(string refreshToken) : IRequest<Result<User>>;
+}
---
+++ IdentityService/Shared/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using IdentityService.Features.Authantication;
+using IdentityService.Shared.Entities;
+using System.Threading.Tasks;
+
+namespace IdentityService.Shared.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> GenerateTokensAsync(User user);
+        Task<AuthModel> RefreshTokenAsync(string refreshToken);
+        Task<string> GeneratePasswordHashAsync(string realPassword);
+        Task<bool> CheckPasswordAsync(User user, string password);
+
+    }
+}
